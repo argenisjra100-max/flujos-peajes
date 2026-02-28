ed845fe5b5ac9b8c36f1f8ee71bfd4d4b2095210
Jose Rangel <jose.rangel@gopass.local>
2026-02-28 12:54:24 -0500

    Organización de archivos por separados js y css

---
+++ script.js
@@ -0,0 +1,15 @@
+
+    //  INICIALIZACIÓN MERMAID
+
+
+/*
+  Configuración de Mermaid:
+  - startOnLoad: renderiza automáticamente
+  - theme: estilo visual uniforme
+  - securityLevel: "loose" permite HTML básico
+*/
+mermaid.initialize({
+    startOnLoad: true,
+    theme: "default",
+    securityLevel: "loose"
+});
